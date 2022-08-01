089ab684cc315a4ae30e8f3845204c52c9e2b4f8
Toni Isotalo <toni.isotalo@profitsoftware.com>
2022-08-01 21:47:40 +0300

    Table removed

--- Server/scripts/tables/location.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS locations (
-    id INT NOT NULL AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    coordinates TEXT NOT NULL,
-    city TEXT NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
